fb54a2d014a6b45ff297395ab0ba6620a70002aa
Jesús Vázquez <34081202+jvazquezrioja@users.noreply.github.com>
2022-04-23 22:00:24 -0500

    Update Form2.vb

--- cajeroOpciones/Form2.vb
+++ cajeroOpciones/Form2.vb
@@ -1,4 +1,4 @@
-Public Class frmCajeroOpciones : Inherits frmCajeroAcceso 'Agregar esto me marca error
+Public Class frmCajeroOpciones 
     Dim conjuntoDeOperaciones() As String
     Private Sub ButtonSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
         Me.Close() 'Cierra el menu de opciones
