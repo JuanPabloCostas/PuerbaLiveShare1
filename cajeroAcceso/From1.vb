dacc4be21ad34c686526f9e10b7e800327976b7f
Jesús Vázquez <34081202+jvazquezrioja@users.noreply.github.com>
2022-04-24 09:22:51 -0500

    Versión 3
    Agregué utilidad para variable global "idioma" y quite lo de inherits

--- cajeroAcceso/From1.vb
+++ cajeroAcceso/From1.vb
@@ -1,16 +1,18 @@
 Public Class frmCajeroAcceso
-    Public Shared salario As Double = Int((5000 * Rnd()) + 1) 'Genera un salario nuevo para cada usuario
-
-    Public Shared idioma As Boolean 'Sí el valor es true entonces esta en español, false esta en inglés
+    Public Shared idioma As Boolean  'Sí el valor es true entonces esta en español, false esta en inglés
 
     Private Sub btnMenuOpciones_Click(sender As Object, e As EventArgs) Handles btnMenuOpciones.Click
         frmCajeroOpciones.Show() 'Abre el siguiente form con las opciones del cajero
     End Sub
 
     Private Sub cbxIdiomas_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbxIdiomas.SelectedIndexChanged
-        If cbxIdiomas.SelectedItem = "Inglés" Then 'Asigna los valores a la variable global de idioma
-            idioma = False                          'dependiendo de la selección del usuario
+        If cbxIdiomas.SelectedItem = "English" Then 'Asigna los valores a la variable global de idioma
+            lblAcceso.Visible = False     'dependiendo de la selección del usuario
+            lblWelcome.Visible = True
+            idioma = False
         ElseIf cbxIdiomas.SelectedItem = "Español" Then
+            lblAcceso.Visible = True
+            lblWelcome.Visible = False
             idioma = True
         End If
     End Sub
